3aa5ff6c90713e509cb12a165dc4963b44c2e7f2
steven <shotta3xd@gmail.com>
2023-07-27 10:56:49 -0400

    Dummy data pushed

---
+++ data.js
@@ -0,0 +1,30 @@
+const candyData = [
+  {
+    id: 1,
+    name: "Chocolate Bar",
+    description: "Delicious milk chocolate bar.",
+    category: "Chocolate",
+    currentPrice: 1.99,
+    stock: 50,
+    imageURL: "https://example.com/chocolate.jpg",
+  },
+  {
+    id: 2,
+    name: "Gummy Bears",
+    description: "Assorted fruity gummy bears.",
+    category: "Gummies",
+    currentPrice: 0.99,
+    stock: 100,
+    imageURL: "https://example.com/gummy-bears.jpg",
+  },
+  {
+    id: 3,
+    name: "Hard Candy",
+    description: "Assorted flavors of hard candy.",
+    category: "Hard Candy",
+    currentPrice: 2.49,
+    stock: 30,
+    imageURL: "https://example.com/hard-candy.jpg",
+  },
+  // Add more candy objects as needed
+];
